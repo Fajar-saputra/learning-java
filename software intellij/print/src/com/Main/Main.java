ea37d24c10a10575976acac41535e26752a1d780
Fajar Saputra <fajarsaputra2305@gmail.com>
2024-10-31 00:34:47 +0700

    add folder software intlij

---
+++ software intellij/print/src/com/Main/Main.java
@@ -0,0 +1,13 @@
+package com.Main;
+
+public class Main   {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+        System.out.println("Ini contoh print dengan enter");
+        System.out.println("Ini contoh print dengan enterss");
+        System.out.println("Ini contoh print dengan enter");
+        System.out.print("Ini contoh print tanpa enter");
+        System.out.print("Ini contoh print tanpa enter");
+//     contoh komentar
+    }
+}
---
+++ software intellij/variable/src/com/Main/Main.java
@@ -0,0 +1,9 @@
+package com.Main;
+
+public class Main {
+    public  static  void main(String[] args){
+        String nama = "Otong";
+        int angka  = 123;
+        System.out.println(nama);
+    }
+}
