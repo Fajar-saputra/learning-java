73ac4d0b7990d4f34636145630559d42f744bd6f
Fajar Saputra <fajarsaputra2305@gmail.com>
2024-11-01 17:34:06 +0700

    delete files and modif file variable

--- software intellij/variable/src/com/Main/Main.java
+++ software intellij/variable/src/com/Main/Main.java
@@ -2,8 +2,65 @@ package com.Main;
 
 public class Main {
     public  static  void main(String[] args){
-        String nama = "Otong";
-        int angka  = 123;
-        System.out.println(nama);
+//        varible, deklarasi, assigment, tipe data
+
+//      Integer
+        System.out.println("\n");
+        int x = 10;
+        System.out.println("Nilai dari Integer x = " + x);
+        System.out.println("Nilai Max dari Integer = " + Integer.MAX_VALUE);
+        System.out.println("Nilai Min dari Integer = " + Integer.MIN_VALUE);
+        System.out.println("Ukuran dari Integer = " + Integer.SIZE + " bit");
+        System.out.println("\n");
+
+//      short
+        short y = 10;
+        System.out.println("Nilai dari Short y = " + y);
+        System.out.println("Nilai Max dari Short = " + Short.MAX_VALUE);
+        System.out.println("Nilai Min dari Short = " + Short.MIN_VALUE);
+        System.out.println("Ukuran dari Short = " + Short.SIZE + " bit");
+        System.out.println("\n");
+
+//      long
+        long z = 10l;
+        System.out.println("Nilai dari Long z = " + z);
+        System.out.println("Nilai Max dari Long = " + Long.MAX_VALUE);
+        System.out.println("Nilai Min dari Long = " + Long.MIN_VALUE);
+        System.out.println("Ukuran dari Long = " + Long.SIZE + " bit");
+        System.out.println("\n");
+
+//      float
+        float a = 10.3f;
+        System.out.println("Nilai dari Float a = " + a);
+        System.out.println("Nilai Max dari Float = " + Float.MAX_VALUE);
+        System.out.println("Nilai Min dari Float = " + Float.MIN_VALUE);
+        System.out.println("Ukuran dari Float = " + Float.SIZE + " bit");
+        System.out.println("\n");
+
+//      double
+        double d = 10.36d;
+        System.out.println("Nilai dari Double d = " + d);
+        System.out.println("Nilai Max dari Double = " + Double.MAX_VALUE);
+        System.out.println("Nilai Min dari Double = " + Double.MIN_VALUE);
+        System.out.println("Ukuran dari Double = " + Double.SIZE + " bit");
+        System.out.println("\n");
+
+//      byte
+        byte w = 10;
+        System.out.println("Nilai dari byte a = " + w);
+        System.out.println("\n");
+
+//      char
+        char c = 'c';
+        System.out.println("Nilai dari Character a = " + c);
+        System.out.println("Nilai Max dari Character = " + Character.MAX_VALUE);
+        System.out.println("Nilai Min dari Character = " + Character.MIN_VALUE);
+        System.out.println("Ukuran dari Character = " + Character.SIZE + " bit");
+        System.out.println("Ukuran dari Character = " + Character.BYTES + " bytes");
+        System.out.println("\n");
+
+//        boolean
+        boolean benar = true;
+        System.out.println("Ini contoh boolean = " + benar);
     }
 }
