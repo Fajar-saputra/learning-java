b80758879fd8d6579d1e94389bafc043bbfb46be
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-02-04 22:59:26 +0700

    update for oop java

---
+++ dasar-java/16-scanner/src/main/main.java
@@ -0,0 +1,25 @@
+package main;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("tess aja");
+    }
+    public  static  double average(int[] numbers){
+        int sum = 0;
+        for (int number : numbers){
+            sum += number;
+        }
+
+        double avg = (double) sum / numbers.length;
+        return  ave;
+    }
+
+    average(1,2,3,4,5);
+
+}
+
+
+
+
